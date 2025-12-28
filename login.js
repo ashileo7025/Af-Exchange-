fff2acbc15f04b8c4967455d16df4442b67615e6
ashileo7025 <ashileo7025@gmail.com>
2025-12-28 07:58:10 -0800

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,7 @@
-// Firebase config (Firebase console se mila hua)
+<script type="module">
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, updatePassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAbuH_r-QjjCD3G5E_oHdVrpR5-0M21Pmo",
   authDomain: "af---exchange.firebaseapp.com",
@@ -8,21 +11,32 @@ const firebaseConfig = {
   appId: "1:42976263940:web:8742baa4943128dbfa1a51"
 };
 
-// Firebase initialize
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-// Login function
-function login() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  auth.signInWithEmailAndPassword(email, password)
-    .then(() => {
-      window.location.href = "dashboard.html";
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
+// LOGIN
+window.login = function(email, password){
+  signInWithEmailAndPassword(auth, email, password)
+  .then(()=> window.location.href="dashboard.html")
+  .catch(e=>alert(e.message));
 }
 
+// REGISTER
+window.register = function(email, password){
+  createUserWithEmailAndPassword(auth, email, password)
+  .then(()=> alert("Account created"))
+  .catch(e=>alert(e.message));
+}
+
+// LOGOUT
+window.logout = function(){
+  signOut(auth).then(()=> window.location.href="index.html");
+}
+
+// CHANGE PASSWORD
+window.changePass = function(newPass){
+  updatePassword(auth.currentUser, newPass)
+  .then(()=> alert("Password updated"))
+  .catch(e=>alert(e.message));
+}
+</script>
